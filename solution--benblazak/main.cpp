d78134246db0825b79abe5cdabe0bbf926cfa786
ayl02410 <ayl02410@users.noreply.github.com>
2015-12-06 14:08:17 -0800

    Added name

--- solution--benblazak/main.cpp
+++ solution--benblazak/main.cpp
@@ -1,5 +1,6 @@
 /* ----------------------------------------------------------------------------
  * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
+                    2015 Alex Liao  <ycl@csu.fullerton.edu>
  * Released under the [MIT License] (http://opensource.org/licenses/MIT)
  * ------------------------------------------------------------------------- */
 
